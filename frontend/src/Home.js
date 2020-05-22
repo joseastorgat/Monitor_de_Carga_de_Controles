4a9b72b7b32bea13b67b5e065807d524e6298726
José Astorga <jose.n.astorga.tobar@gmail.com>
2020-05-21 21:34:49 -0400

    [ADD] Manejo de Links privados Prueba en Home

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -7,9 +7,25 @@ import {nuevo_ramo ,editar_ramo, lista_ramos} from "./Ramo/index_ramo";
 import administrar from "./Administrar";
 
 import { Link } from "react-router-dom";
+import PrivateRoute from "./common/PrivateRoute"
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+
 
 class Bloque_Calendario extends React.Component {
+  
+  static propTypes = {
+    auth: PropTypes.object.isRequired,
+  };
   render() {
+    const { isAuthenticated} = this.props.auth;
+    const authLinks = (
+      <Link to="/administrar">
+        <div className="rectangulo_azul" style={{ backgroundColor: "gray"}} >Administrar</div>
+      </Link>
+    );
+
     return (
       <div>
       <div className="centrar">
@@ -19,50 +35,52 @@ class Bloque_Calendario extends React.Component {
       <div className="centrar">
         <div className="rectangulo_azul" >Ver Semestre Otoño 2020</div>
         <div className="rectangulo_azul" >Ver Semestre Primavera 2020</div>
-        <div className="rectangulo_azul" >Ver Semestre Primavera 2020</div>
-        <div className="rectangulo_azul" >Ver Semestre Primavera 2020</div>
-        <Link to="/administrar">
-          <div className="rectangulo_azul" style={{ backgroundColor: "gray"}} >Administrar</div>
-        </Link>
+        {
+        /* <div className="rectangulo_azul" >Ver Semestre Primavera 2020</div>
+        <div className="rectangulo_azul" >Ver Semestre Primavera 2020</div> */}
+        
+        {isAuthenticated ? authLinks: '' }
       </div>
       </div>
     );
   }
 }
 
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+const Bloque_Calendario_con = connect(mapStateToProps)(Bloque_Calendario);
+
 export default class Home extends React.Component {
-render() {
+  render() {
     return (
     <div>
-        <Header />
-        
+      <Header />
       <div>
         <Switch>
-          <Route exact path="/" component={Bloque_Calendario} />
+          <Route exact path="/" component={Bloque_Calendario_con} />
 
-           {/* VISTAS DE ADMINISTRAR */}
-           <Route exact path="/administrar" component={administrar} />
+          {/* VISTAS DE ADMINISTRAR */}
+          <PrivateRoute exact path="/administrar" component={administrar} />
 
           {/* VISTAS DE SEMESTRE */}
-          <Route exact path="/semestres" component={lista_semestres} />
-          <Route exact path="/semestres/nuevo_semestre" component={nuevo_semestre} />
-          <Route exact path="/semestres/:ano/:semestre" component={ver_semestre}  />
-          <Route exact path="/semestres/:ano/:semestre/editar" component={editar_semestre} />
+          <PrivateRoute exact path="/semestres" component={lista_semestres} />
+          <PrivateRoute exact path="/semestres/nuevo_semestre" component={nuevo_semestre} />
+          <PrivateRoute exact path="/semestres/:ano/:semestre" component={ver_semestre}  />
+          <PrivateRoute exact path="/semestres/:ano/:semestre/editar" component={editar_semestre} />
 
           {/* VISTAS DE CURSO */}
-          <Route exact path="/semestres/:ano/:semestre/nuevo_curso" component={nuevo_curso} />
-          <Route exact path="/semestres/:ano/:semestre/:cod/:seccion" component={ver_curso} />
-          <Route exact  path="/semestres/:ano/:semestre/:cod/:seccion/editar"  component={editar_curso}  />
+          <PrivateRoute exact path="/semestres/:ano/:semestre/nuevo_curso" component={nuevo_curso} />
+          <PrivateRoute exact path="/semestres/:ano/:semestre/:cod/:seccion" component={ver_curso} />
+          <PrivateRoute exact  path="/semestres/:ano/:semestre/:cod/:seccion/editar"  component={editar_curso}  />
 
           {/* VISTAS DE RAMO */}
-          <Route exact path="/ramos" component={lista_ramos} />
-          <Route exact path="/ramos/nuevo_ramo" component={nuevo_ramo} />
-          <Route exact path="/ramos/:id/editar" component={editar_ramo} />
+          <PrivateRoute exact path="/ramos" component={lista_ramos} />
+          <PrivateRoute exact path="/ramos/nuevo_ramo" component={nuevo_ramo} />
+          <PrivateRoute exact path="/ramos/:id/editar" component={editar_ramo} />
         </Switch>
       </div>
-
-
-
       </div>
     );
   }
