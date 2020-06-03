e2963ed11541c089a56627e4290381f756e91d05
nikom1912 <nikom_1912@hotmail.com>
2020-06-03 00:25:02 -0400

    [CHG] Export de nuevo archivo de evaluaciones

--- frontend/src/Evaluacion/index_evaluacion.js
+++ frontend/src/Evaluacion/index_evaluacion.js
@@ -1,3 +1,3 @@
-export { default as nueva_evaluacion } from "./nueva_evaluacion";
+export { default as evaluaciones } from "./evaluaciones";
 export { default as lista_evaluaciones } from "./lista_evaluaciones";
 export { default as editar_evaluacion } from "./editar_evaluacion";
