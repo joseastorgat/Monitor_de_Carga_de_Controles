2d38293e1bca27942c1b101ad4d23af58fabdc9e
nikom1912 <nikom_1912@hotmail.com>
2020-06-19 13:14:11 -0400

    [ADD] Eliminar semestre con confirmacion año y periodo

--- frontend/src/semestre/lista_semestres.js
+++ frontend/src/semestre/lista_semestres.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {   Alert,Button,   Container,   Col,   Row,   Form,   FormControl,   InputGroup } from "react-bootstrap";
+import {   Alert,Button, Modal,  Container,   Col,   Row,   Form,   FormControl,   InputGroup } from "react-bootstrap";
 import ViewTitle from "../common/ViewTitle";
 import { Link } from "react-router-dom";
 import DeleteModal from "../common/DeleteModal";
@@ -69,11 +69,41 @@ export class lista_semestre extends React.Component {
     this.setState({ showModal: false, semestrePorEliminar: null });
   }
 
+  async handleDelete() {
+    let e = this.state.semestrePorEliminar.id
+    console.log(e)
+    const url = `http://127.0.0.1:8000/api/semestres/${e}/`
+    let options = {
+      method: 'DELETE',
+      url: url,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${this.props.auth.token}`
+      }
+    }
+    axios(options)
+      .then( (res) => {
+        this.setState({
+          showModal: false,
+          semestrePorEliminar: null
+        });
+        this.fetchSemestres();
+      })
+      .catch( (err) => {
+        console.log(err);
+        alert("[ERROR] No se pudo eliminar el semestre! ");
+        this.setState({
+          showModal: false,
+          semestrePorEliminar: null
+        });
+      });
+  }
+
   render() {
       return (
         <main>
-        <DeleteModal
-          msg={this.deleteModalMsg}
+        <DeleteSemestreModal
+          semestre={this.state.semestrePorEliminar}
           show={this.state.showModal}
           handleCancel={() => this.handleCancel()}
           handleDelete={() => this.handleDelete()}
@@ -119,6 +149,79 @@ export class lista_semestre extends React.Component {
     }
   }
 
+  class DeleteSemestreModal extends React.Component{
+    constructor(props){
+      super(props)
+      this.state = {
+        eliminar_año : "",
+        eliminar_periodo : "",
+        semestre_deleted : false
+      }
+      
+    }
+    onChange = e => {
+      this.setState({
+        [e.target.name]: 
+        e.target.value
+      })
+    };
+
+    deleteValidation(semestre){
+      console.log(this.state.eliminar_año)
+      if(this.state.eliminar_año == "" || this.state.eliminar_periodo == ""){
+        alert("Debe ingresar tanto el año como el periodo del semestre que esta eliminando")
+        return false
+      }
+      var periodo = "" 
+      if (this.state.eliminar_periodo.toLowerCase() == "primavera"){
+        periodo = 2
+      }
+      if(this.state.eliminar_periodo.toLowerCase() == "otoño"){
+        periodo = 1
+      }
+      if(semestre.año == this.state.eliminar_año && semestre.periodo == periodo){
+        this.state.semestre_deleted = true
+        return true
+      }
+      alert("Los datos ingresados no coinciden con el semestre que se desea eliminar")
+      return false
+    }
+    render(){
+      const{show, handleCancel, handleDelete, semestre, } = this.props;
+      return (
+        <Modal show={show} onHide={() => handleCancel()}>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Confirmación de eliminación
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <label><font color="red">[ADVERTENCIA]</font> Eliminar un semestre eliminara los cursos y evaluaciones asociadas, para confirmar la eliminacion del semestre escogido ingrese el año y el periodo del semestre.</label>
+            <div class="col-sm-6" >
+              <label >Año</label>
+            </div>
+            <div class="col-sm-9" >
+              <input type="text" className="form-control" name="eliminar_año"  onChange={this.onChange} style={{textAlignLast:'center'}} />
+            </div>
+            <div class="col-sm-6" >
+              <label> Periodo (Primavera u Otoño)</label>
+            </div>
+            <div class="col-sm-9" >
+              <input type="text" className="form-control" name="eliminar_periodo"  onChange={this.onChange} style={{textAlignLast:'center'}} />
+            </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => handleCancel()}>
+            Cancelar
+          </Button>
+          <Button variant="danger" onClick={() => this.deleteValidation(semestre) ? handleDelete() : this.state.semestre_deleted = false}>
+            Eliminar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      )
+    }
+  } 
 
   class SemesterItem extends React.Component {
     constructor(props) {
