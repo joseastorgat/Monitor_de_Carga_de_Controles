79ffeebcdea992e2e420540ba882543728bf826f
plt1994 <patlopez@ug.uchile.cl>
2020-07-06 21:08:27 -0400

    [FIX] se corrigen errores varios al agregar semestre mediante excel

--- api/utils.py
+++ api/utils.py
@@ -1,30 +1,104 @@
 from api.models import Semestre, Ramo, Profesor, Curso, Evaluacion
 
+EVALS = 3
+CURSOS = 2
+FULL = 1
+
+
+def create_semester(semester, level=EVALS):
+    # response vacio, solo con warnings
+    response = {'warning': []}
+    response['prof status'] = []
+    response['curso status'] = []
+    response['ramos status'] = []
+    try:
+        if level == EVALS:
+            response['eval status'] = []
+        # verificar existencia se semestre
+        sem = Semestre.objects.filter(
+            año=semester['year'], periodo=semester['period'])
+        if sem:
+            if level == FULL:  # si se quiere crear un semestre totalmente nuevo, pero ya está.
+                return {'status': 'Semestre ya existe en base de datos. ¿Desea \
+completar un semestre ya existente?'}
+            else:
+                sem = sem.get()
+        # sino, crear un semestre nuevo
+        else:
+            sem = Semestre.objects.create(
+                año=semester['year'], inicio=semester['start'],
+                fin=semester['finish'],
+                periodo=semester['period'], estado=3)
+        # ingresar cada curso al semestre
+        for curso in semester["cursos"]:
+            # hacer filtro por código de ramo, para evitar confución de nombre
+            ramo = Ramo.objects.filter(codigo=curso['codigo'])
+            if not ramo:
+                # si el ramo no existe, se crea uno nuevo, se agrega al response
+                ramo = Ramo.objects.create(
+                    nombre=curso['nombre_ramo'], codigo=curso['codigo'],
+                    semestre_malla=curso['semestre_malla'])
+                response['ramos status'].append(f'{ramo} agregado a la base de datos de ramos.')
+            else:
+                ramo = ramo.get()
+                response['ramos status'].append(f'{ramo.codigo} ya existe en la base de datos \
+con el nombre {ramo}')
+            print(ramo)
+            # verificar existencia del curso
+            curso_inst = Curso.objects.filter(
+                ramo=ramo, semestre=sem, seccion=curso['seccion'])
+            if curso_inst:
+                # si curso ya existía en el semestre, se ignoran sus evaluaciones.
+                # Se deja un warning.
+                if level == CURSOS:
+                    response['warning'].append(
+                        f'curso {str(curso_inst.get())} ya existente, información \
+no agregada.')
+                    continue
+                else:
+                    curso_inst = curso_inst.get()
+            else:
+                curso_inst = Curso.objects.create(
+                    ramo=ramo, semestre=sem, seccion=curso['seccion'])
+                response['curso status'].append(f'{curso_inst}, agregado correctamente.')
+            for profe in curso['profesor']:
+                # profe_nombre, profe_apellido = profe['nombre'].replace(
+                #     ' ', '').split('.')
+                # profe_name = profe_nombre.capitalize()+'.' + \
+                #     profe_apellido.capitalize()
+                prof, prof_created = Profesor.objects.get_or_create(
+                    nombre=profe['nombre'])
+                if prof_created:
+                    response['prof status'].append(f'{prof} no encontrado en base \
+de datos, se ha agregado una nueva entrada para {prof}')
+                curso_inst.profesor.add(prof)
+            for eval in curso['evaluaciones']:
+                eval_inst, eval_is_created = Evaluacion.objects.get_or_create(
+                    fecha=eval['fecha'], tipo=eval['tipo'], titulo=eval['titulo'],
+                    curso=curso_inst)
+                if level == EVALS:
+                    if eval_is_created:
+                        response['eval status'].append(
+                            f'{eval_inst} agregada correctamente')
+                    else:
+                        response['warning'].append(
+                            f'{eval_inst} ya existente, no agregada')
+        response['status'] = "Todo correcto!"
+    except Exception as e:
+        print(e)
+        response['status'] = 'Error no identificado, se ha detenido\
+ la carga del semestre en un punto indeterminado'
+    return response
+    # return {'status': 'Semestre creado!'}
+
 
 def create_full_semester(semester):
-    sem = Semestre.objects.create(
-        año=semester['year'], inicio=semester['start'], fin=semester['finish'], periodo=semester['period'], estado=3)
-    for curso in semester["cursos"]:
-        ramo = Ramo.objects.filter(codigo=curso['codigo'])
-        if not ramo:
-            ramo = Ramo.objects.create(
-                nombre=curso['nombre_ramo'], codigo=curso['codigo'],
-                semestre_malla=curso['semestre_malla'])
-        else:
-            ramo = ramo.get()
-        print(ramo)
-        curso_inst = Curso.objects.create(
-            ramo=ramo, semestre=sem, seccion=curso['seccion'])
-        for profe in curso['profesor']:
-            # profe_nombre, profe_apellido = profe['nombre'].replace(
-            #     ' ', '').split('.')
-            # profe_name = profe_nombre.capitalize()+'.' + \
-            #     profe_apellido.capitalize()
-            prof, prof_created = Profesor.objects.get_or_create(
-                nombre=profe['nombre'])
-            curso_inst.profesor.add(prof)
-        for eval in curso['evaluaciones']:
-            Evaluacion.objects.create(
-                fecha=eval['fecha'], tipo=eval['tipo'], titulo=eval['titulo'],
-                curso=curso_inst)
-    print('listo')
+    return create_semester(semester, level=FULL)
+
+
+def add_courses_to_semester(semester):
+    return create_semester(semester, level=CURSOS)
+
+
+def add_evals_to_semester(semester):
+    return create_semester(semester, level=EVALS)
--- api/views.py
+++ api/views.py
@@ -17,7 +17,8 @@ from rest_framework.request import Request
 from rest_framework.parsers import FormParser, MultiPartParser
 
 from api.parser import parse_excel
-from api.utils import create_full_semester
+from api.utils import create_full_semester, add_courses_to_semester,\
+    add_evals_to_semester
 
 
 class SemestreViewSet(viewsets.ModelViewSet):
@@ -77,9 +78,63 @@ class SemestreViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             bin_file = request.data['file']
             parsed = parse_excel(bin_file)
-            create_full_semester(parsed)
+            response = create_full_semester(parsed)
+            print(response)
             # ver bien el response a dar
-            return Response({'status': 'Semestre creado!'})
+            return Response(response)
+        else:
+            print(serializer.errors)
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False,
+            methods=['POST'],
+            serializer_class=SemestreFileSerializer,
+            permission_classes=[],  # [permissions.IsAuthenticatedOrReadOnly],
+            parser_classes=[
+                FormParser,
+                MultiPartParser,
+            ]
+            )
+    def from_xlsx2(self, request, *args, **kwargs):
+        '''
+        Crear semestre mediante archivo excel xlsx
+        '''
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            bin_file = request.data['file']
+            parsed = parse_excel(bin_file)
+            response = add_courses_to_semester(parsed)
+            print(response)
+            # ver bien el response a dar
+            return Response(response)
+        else:
+            print(serializer.errors)
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    
+    @action(detail=False,
+            methods=['POST'],
+            serializer_class=SemestreFileSerializer,
+            permission_classes=[],  # [permissions.IsAuthenticatedOrReadOnly],
+            parser_classes=[
+                FormParser,
+                MultiPartParser,
+            ]
+            )
+    def from_xlsx3(self, request, *args, **kwargs):
+        '''
+        Crear semestre mediante archivo excel xlsx
+        '''
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            bin_file = request.data['file']
+            parsed = parse_excel(bin_file)
+            response = add_evals_to_semester(parsed)
+            print(response)
+            # ver bien el response a dar
+            return Response(response)
         else:
             print(serializer.errors)
             return Response(serializer.errors,
